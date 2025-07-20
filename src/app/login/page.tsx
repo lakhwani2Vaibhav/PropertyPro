3cddb8d18684245e5d7486cf767052489d3236f2
Vaibhav Lakhwani <vaibhavlakhwanivns@gmail.com>
2025-07-20 07:58:34 +0000

    [13:26:53.459] Running build in Washington, D.C., USA (East) – iad1 [13:

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import { Suspense } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
